24216bac892ce7bc697421896150a089f9d75acb
gchoy <gabriela.choy@gmail.com>
2017-02-26 19:33:03 -0800

    Logic of tic tac toe added

--- js/app.js
+++ js/app.js
@@ -2,5 +2,73 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+var mark = function(numClicks,box){
+     if (numClicks%2===0  && $(box).text() =="blank"){
+       $(box).text("X");
+     } else if(numClicks%2 ===1 && $(box).text()=="blank"){
+       $(box).text("O");
+     }
+
+   }
+
+
+   var numClicks = 0;
+
+   $("div#a.col-md-4.box").on("click",function(){
+     console.log("I clicked div#a.col-md-4.box")
+     numClicks += 1;
+     var box = "div#a.col-md-4.box";
+     mark(numClicks,box);
+   });
+
+   $("div#b.col-md-4.box").on("click",function(){
+     console.log("I clicked div#b.col-md-4.box")
+     numClicks += 1;
+     var box = "div#b.col-md-4.box";
+     mark(numClicks,box);
+   });
+   $("div#c.col-md-4.box").on("click",function(){
+     console.log("I clicked div#c.col-md-4.box")
+     numClicks += 1;
+     var box = "div#c.col-md-4.box";
+     mark(numClicks,box);
+   });
+   $("div#d.col-md-4.box").on("click",function(){
+     console.log("I clicked div#d.col-md-4.box")
+     numClicks += 1;
+     var box = "div#d.col-md-4.box";
+     mark(numClicks,box);
+   });
+   $("div#e.col-md-4.box").on("click",function(){
+     console.log("I clicked div#e.col-md-4.box")
+     numClicks += 1;
+     var box = "div#e.col-md-4.box";
+     mark(numClicks,box);
+   });
+   $("div#f.col-md-4.box").on("click",function(){
+     console.log("I clicked div#f.col-md-4.box")
+     numClicks += 1;
+     var box = "div#f.col-md-4.box";
+     mark(numClicks,box);
+   });
+   $("div#g.col-md-4.box").on("click",function(){
+     console.log("I clicked div#g.col-md-4.box")
+     numClicks += 1;
+     var box = "div#g.col-md-4.box";
+     mark(numClicks,box);
+   });
+   $("div#h.col-md-4.box").on("click",function(){
+     console.log("I clicked div#h.col-md-4.box")
+     numClicks += 1;
+     var box = "div#h.col-md-4.box";
+     mark(numClicks,box);
+   });
+   $("div#i.col-md-4.box").on("click",function(){
+     console.log("I clicked div#i.col-md-4.box")
+     numClicks += 1;
+     var box = "div#i.col-md-4.box";
+     mark(numClicks,box);
+   });
+
 
 });
